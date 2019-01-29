0c3fb75a3f3b34a233dc207fb6f1adc16a887c3a
J Jones <jlssmmns93@gmail.com>
2019-01-29 11:55:11 -0500

    Add project files.

---
+++ Resolutions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Resolutions.Startup))]
+namespace Resolutions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
