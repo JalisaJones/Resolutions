af16488e5c73479c412b4f518aba66e074747315
J Jones <jlssmmns93@gmail.com>
2019-01-30 14:30:34 -0500

    first commit

--- Resolutions/App_Start/Startup.Auth.cs
+++ Resolutions/App_Start/Startup.Auth.cs
@@ -5,6 +5,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
+using Resolutions.Data;
 using Resolutions.Models;
 
 namespace Resolutions
